c6aa757bd9cdec9408842f223b57769e4666b025
shishir <shisirsharma22@gmail.com>
2024-05-29 11:39:07 +0545

    added custom theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:sajilo_sales/screens/splash_screen.dart';
+import 'package:sajilo_sales/theme/theme_data.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData darkTheme = ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: Colors.deepPurple,
-      primaryColorDark: Colors.deepPurple,
-      hintColor: Colors.amber,
-      scaffoldBackgroundColor: Colors.black,
-      // Customize more properties as needed
-    );
     return MaterialApp(
-      theme: darkTheme,
+      theme: getApplicationTheme(),
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
