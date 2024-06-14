6071017e66bb870d9bf32b72c4381117515eb30b
shishir <shisirsharma22@gmail.com>
2024-06-14 18:16:47 +0545

    added failure and snackbar

--- lib/core/common/custom_snackbar.dart
+++ lib/core/common/custom_snackbar.dart
@@ -1,3 +1,21 @@
-class CustomSnackbar {
-  
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:sajilo_sales/app/navigator_key/navigator_key.dart';
+
+showMySnackBar({
+  required String message,
+  Color? color,
+}) {
+  ScaffoldMessenger.of(
+    // We already created this navigator key in the navigator_key.dart file
+    AppNavigator.navigatorKey.currentState!.context,
+  ).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      backgroundColor: color ?? Colors.green,
+      duration: const Duration(seconds: 1),
+      behavior: SnackBarBehavior.floating,
+    ),
+  );
 }
