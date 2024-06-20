139b842954575afb7978306058f0d84ca77b02d3
shishir <shisirsharma22@gmail.com>
2024-06-20 23:03:44 +0545

    added remote repo

--- lib/features/auth/data/repository/auth_remote_repository.dart
+++ lib/features/auth/data/repository/auth_remote_repository.dart
@@ -1,35 +1,27 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:dartz/dartz.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:sajilo_sales/core/failure/failure.dart';
-// import 'package:sajilo_sales/features/auth/data/data_source/remote/auth_remote_data_source.dart';
-// import 'package:sajilo_sales/features/auth/domain/entity/auth_entity.dart';
-// import 'package:sajilo_sales/features/auth/domain/repository/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sajilo_sales/core/failure/failure.dart';
+import 'package:sajilo_sales/features/auth/data/data_source/remote/auth_remote_data_source.dart';
+import 'package:sajilo_sales/features/auth/domain/entity/auth_entity.dart';
+import 'package:sajilo_sales/features/auth/domain/repository/auth_repository.dart';
 
+final authRemoteRepositoryProvider = Provider<IAuthRepository>((ref) {
+  return AuthRemoteRepository(ref.read(authRemoteDataSourceProvider));
+});
 
-// final authRemoteRepositoryProvider = Provider<IAuthRepository>((ref) {
-//   return AuthRemoteRepository(ref.read(authRemoteDataSourceProvider));
-// });
+class AuthRemoteRepository implements IAuthRepository {
+  final AuthRemoteDataSource _authRemoteDataSource;
 
-// class AuthRemoteRepository implements IAuthRepository {
-//   final AuthRemoteDataSource _authRemoteDataSource;
+  AuthRemoteRepository(this._authRemoteDataSource);
+  @override
+  Future<Either<Failure, bool>> loginStudent(String email, String password) {
+    return _authRemoteDataSource.loginUser(email, password);
+  }
 
-//   AuthRemoteRepository(this._authRemoteDataSource);
-//   @override
-//   Future<Either<Failure, bool>> loginStudent(String username, String password) {
-//     // TODO: implement loginStudent
-//     throw UnimplementedError();
-//   }
-
-//   @override
-//   Future<Either<Failure, bool>> registerStudent(AuthEntity student) {
-//     // TODO: implement registerStudent
-//     throw UnimplementedError();
-//   }
-
-//   @override
-//   Future<Either<Failure, String>> uploadProfilePicture(File file) {
-//     return _authRemoteDataSource.uploadProfilePicture(file);
-//   }
-// }
+  @override
+  Future<Either<Failure, bool>> registerStudent(AuthEntity user) {
+    return _authRemoteDataSource.registerUser(user);
+  }
+}
