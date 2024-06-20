a494fcc025ca3f41097bc1d8e91312aec60f30fe
shishir <shisirsharma22@gmail.com>
2024-06-20 23:03:10 +0545

    added remote data source

--- lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -1,125 +1,88 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:dartz/dartz.dart';
-// import 'package:dio/dio.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:student_management_starter/app/constants/api_endpoint.dart';
-// import 'package:student_management_starter/core/failure/failure.dart';
-// import 'package:student_management_starter/core/networking/remote/http_service.dart';
-// import 'package:student_management_starter/features/auth/domain/entity/auth_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sajilo_sales/app/constants/api_endpoint.dart';
+import 'package:sajilo_sales/core/Networking/remote/http_service.dart';
+import 'package:sajilo_sales/core/failure/failure.dart';
+import 'package:sajilo_sales/core/shared_prefs/user_shared_prefs.dart';
+import 'package:sajilo_sales/features/auth/data/model/auth_api_model.dart';
+import 'package:sajilo_sales/features/auth/domain/entity/auth_entity.dart';
 
-// final authRemoteDataSourceProvider = Provider(
-//   (ref) => AuthRemoteDataSource(
-//     dio: ref.read(httpServiceProvider),
-//   ),
-// );
+final authRemoteDataSourceProvider = Provider(
+  (ref) => AuthRemoteDataSource(
+      authApiModel: ref.read(authApiModelProvider),
+      dio: ref.read(httpServiceProvider),
+      userSharedPrefs: ref.read(userSharedPrefsProvider)),
+);
 
-// class AuthRemoteDataSource {
-//   final Dio dio;
+class AuthRemoteDataSource {
+  final Dio dio;
+  final AuthApiModel authApiModel;
+  final UserSharedPrefs userSharedPrefs;
 
-//   AuthRemoteDataSource({required this.dio});
+  AuthRemoteDataSource(
+      {required this.dio,
+      required this.authApiModel,
+      required this.userSharedPrefs});
 
-//   Future<Either<Failure, bool>> registerStudent(AuthEntity student) async {
-//     try {
-//       Response response = await dio.post(
-//         ApiEndpoints.register,
-//         data: {
-//           "fname": student.fname,
-//           "lname": student.lname,
-//           "phone": student.phone,
-//           "image": student.image,
-//           "username": student.username,
-//           "password": student.password,
-//           "batch": student.batch.batchId,
-//           // "course": ["6489a5908dbc6d39719ec19c", "6489a5968dbc6d39719ec19e"]
-//           "course": student.courses.map((e) => e.courseId).toList(),
-//         },
-//       );
-//       if (response.statusCode == 200) {
-//         return const Right(true);
-//       } else {
-//         return Left(
-//           Failure(
-//             error: response.data["message"],
-//             statusCode: response.statusCode.toString(),
-//           ),
-//         );
-//       }
-//     } on DioException catch (e) {
-//       return Left(
-//         Failure(
-//           error: e.error.toString(),
-//           statusCode: e.response?.statusCode.toString() ?? '0',
-//         ),
-//       );
-//     }
-//   }
+  Future<Either<Failure, bool>> registerUser(AuthEntity user) async {
+    try {
+      Response response = await dio.post(ApiEndpoints.register,
+          data: (authApiModel.fromEntity(user).toJson()));
+      if (response.statusCode == 200) {
+        return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
 
-//   // Upload image using multipart
-//   Future<Either<Failure, String>> uploadProfilePicture(
-//     File image,
-//   ) async {
-//     try {
-//       // Extract name from path
-//       // c:/user/username/pictures/image.png
-//       String fileName = image.path.split('/').last;
-//       print("filename" + fileName);
-//       FormData formData = FormData.fromMap(
-//         {
-//           'profilePicture': await MultipartFile.fromFile(
-//             image.path,
-//             filename: fileName,
-//           ),
-//         },
-//       );
-
-//       Response response = await dio.post(
-//         ApiEndpoints.uploadImage,
-//         data: formData,
-//       );
-
-//       return Right(response.data["data"]);
-//     } on DioException catch (e) {
-//       return Left(
-//         Failure(
-//           error: e.error.toString(),
-//           statusCode: e.response?.statusCode.toString() ?? '0',
-//         ),
-//       );
-//     }
-//   }
-
-//   Future<Either<Failure, bool>> loginStudent(
-//     String username,
-//     String password,
-//   ) async {
-//     try {
-//       Response response = await dio.post(
-//         ApiEndpoints.login,
-//         data: {
-//           "username": username,
-//           "password": password,
-//         },
-//       );
-//       if (response.statusCode == 200) {
-//         // retrieve token
-//         String token = response.data["token"];
-//         return const Right(true);
-//       } else {
-//         return Left(
-//           Failure(
-//             error: response.data["message"],
-//             statusCode: response.statusCode.toString(),
-//           ),
-//         );
-//       }
-//     } on DioException catch (e) {
-//       return Left(
-//         Failure(
-//           error: e.error.toString(),
-//           statusCode: e.response?.statusCode.toString() ?? '0',
-//         ),
-//       );
-//     }
-//   }
-// }
+  Future<Either<Failure, bool>> loginUser(
+    String email,
+    String password,
+  ) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.login,
+        data: {
+          "email": email,
+          "password": password,
+        },
+      );
+      if (response.statusCode == 200) {
+        // retrieve token
+        String token = response.data["token"];
+        await userSharedPrefs.setUserToken(token);
+        return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+}
