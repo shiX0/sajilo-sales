2cea1f89cf793d48c1e2a6c0b4933cba9d92afd9
shishir <shisirsharma22@gmail.com>
2024-06-29 18:33:22 +0545

    added product domain layer

---
+++ lib/features/products/domain/usecases/product_usecase.repository.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sajilo_sales/core/failure/failure.dart';
+import 'package:sajilo_sales/features/products/domain/entity/product_entity.dart';
+import 'package:sajilo_sales/features/products/domain/repository/product_repository.dart';
+
+final productUsecaseProvider = Provider<ProductUsecase>((ref) {
+  return ProductUsecase(ref.read(productRepositoryProvider));
+});
+
+class ProductUsecase {
+  final IProductRepository _productRepository;
+
+  ProductUsecase(this._productRepository);
+
+  Future<Either<Failure, List<ProductEntity>>> getAllProduct(int page) {
+    return _productRepository.getAllProduct(page);
+  }
+}
