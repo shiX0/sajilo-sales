18d56d778aae9104dbbb003f969f9255dad5b498
shishir <shisirsharma22@gmail.com>
2024-06-29 18:32:02 +0545

    chenged dashboard

--- lib/features/home/presentation/view/home_screen.dart
+++ lib/features/home/presentation/view/home_screen.dart
@@ -11,18 +11,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController textEditingController = TextEditingController();
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    int crossAxisCount = screenWidth < 800 ? 2 : 3;
+    // int crossAxisCount = screenWidth < 800 ? 2 : 3;
 
     return Scaffold(
       body: SafeArea(
@@ -31,11 +24,12 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: CustomFormField(
+                icon: const Icon(Icons.search),
                 inputType: TextInputType.text,
                 textEditingController: textEditingController,
                 labelText: 'Search',
               ),
-            ),  
+            ),
             Flexible(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -45,7 +39,9 @@ class _HomeScreenState extends State<HomeScreen> {
                     crossAxisCount: screenWidth < 800 ? 2 : 5,
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
-                    childAspectRatio: 1, // Ensuring the aspect ratio
+                    childAspectRatio: screenWidth < 800
+                        ? 1.3
+                        : 1, // Ensuring the aspect ratio
                   ),
                   itemBuilder: (context, index) {
                     final List<CustomCard> cards = [
@@ -105,34 +101,6 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt),
-            label: 'Orders',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category),
-            label: 'Products',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.people),
-            label: 'Customers',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
       ),
     );
   }
